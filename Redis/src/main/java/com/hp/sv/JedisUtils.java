badcc4dca81efd7b46d635d58a96078d6a33d6c2
Martin Podval <martin@podval.eu>
2013-07-12 11:42:49 +0200

    + init version of working redis app

---
+++ Redis/src/main/java/com/hp/sv/JedisUtils.java
@@ -0,0 +1,15 @@
+package com.hp.sv;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: martin
+ * Date: 7/12/13
+ * Time: 10:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JedisUtils {
+
+    public void TestWriteAndRead() {
+        new Jed
+    }
+}
