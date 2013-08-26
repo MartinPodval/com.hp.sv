5fabfc811422ea09e16ee9ac2626ff34bf6ace76
Martin Podval <martin@podval.eu>
2013-08-26 12:14:24 +0200

    + hamcrest test

--- SV-NG/src/test/java/com/hp/sv/persistence/ModelEntityRepositoryImplTest.java
+++ SV-NG/src/test/java/com/hp/sv/persistence/ModelEntityRepositoryImplTest.java
@@ -1,11 +1,14 @@
 package com.hp.sv.persistence;
 
+import com.hp.sv.model.api.ModelEntity;
 import com.hp.sv.persistence.api.ModelEntityRepository;
 import com.hp.sv.persistence.impl.ModelEntityRepositoryImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(JUnit4.class)
 public class ModelEntityRepositoryImplTest {
@@ -20,5 +23,10 @@ public class ModelEntityRepositoryImplTest {
     @Test(expected = IllegalArgumentException.class)
     public void Throws_When_Not_Valid_Id() {
         repository.GetById(0);
+    }
+
+    @Test
+    public void Returns_Null_If_Id_Is_Valid() {
+        assertThat(repository.GetById(23), is(nullValue()));
     }
 }
